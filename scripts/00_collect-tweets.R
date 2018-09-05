506b807e4ba84b4978f7c6e67708b07862cf3f4d
pvictor <perrier.victor@gmail.com>
2018-09-05 21:30:05 +0200

    update data

--- scripts/00_collect-tweets.R
+++ scripts/00_collect-tweets.R
@@ -32,7 +32,7 @@ twitter_token <- readRDS(file = "datas/twitter_token.rds")
 lignes_RATP <- sprintf("Ligne%d_RATP", 1:14)
 
 # get tweets
-tweets_lignes <- lapply(X = lignes_RATP, FUN = get_timeline, n = 3200)
+tweets_lignes <- lapply(X = lignes_RATP, FUN = get_timeline, n = 3200, token = twitter_token)
 tweets_lignes <- rbindlist(tweets_lignes)
 
 # save data
@@ -46,7 +46,7 @@ saveRDS(tweets_lignes, file = sprintf("datas/raw/tweets_metro_%s.rds", format(Sy
 
 lignes_rer <- c("RER_A", "RERB", "RERC_SNCF", "RERD_SNCF", "RERE_SNCF")
 
-tweets_rer <- lapply(X = lignes_rer, FUN = get_timeline, n = 3200)
+tweets_rer <- lapply(X = lignes_rer, FUN = get_timeline, n = 3200, token = twitter_token)
 tweets_rer <- rbindlist(tweets_rer)
 
 saveRDS(tweets_rer, file = sprintf("datas/raw/tweets_rer_%s.rds", format(Sys.Date())))
@@ -59,7 +59,7 @@ saveRDS(tweets_rer, file = sprintf("datas/raw/tweets_rer_%s.rds", format(Sys.Dat
 
 transilien <- c("LIGNEL_sncf", "LIGNEJ_SNCF", "lignesNetU_SNCF", "LigneH_SNCF", "LIGNEP_SNCF", "LIGNER_SNCF")
 
-tweets_transilien <- lapply(X = transilien, FUN = get_timeline, n = 3200)
+tweets_transilien <- lapply(X = transilien, FUN = get_timeline, n = 3200, token = twitter_token)
 tweets_transilien <- rbindlist(tweets_transilien)
 
 saveRDS(tweets_transilien, file = sprintf("datas/raw/tweets_transilien_%s.rds", format(Sys.Date())))
